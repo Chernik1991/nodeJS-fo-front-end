b62d9287238785c2fa2514f4d873975e651f5423
MAKS <5059585@mail.ru>
2023-05-12 18:28:07 +0300

    Initial commit

---
+++ src/routes/adresses-router.ts
@@ -0,0 +1,16 @@
+import {Request, Response, Router} from 'express';
+
+const addresses = [{id: 1, value: 'Nezavisimosti 12'}, {id: 2, value: 'Selickaga 11'}]
+
+export const addressesRouter = Router()
+addressesRouter.get('/', (req: Request, res: Response) => {
+    res.send(addresses)
+})
+addressesRouter.get('/:id', (req: Request, res: Response) => {
+    const address = addresses.find(a => a.id === +req.params.id)
+    if (address) {
+        res.send(addresses)
+    } else {
+        res.send(404)
+    }
+})
